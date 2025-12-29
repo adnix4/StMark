8bce6da42b92db6fb8debdb9e051b369229851f5
adnix4 <anixdorf@student.cvtc.edu>
2025-12-28 19:39:21 -0600

    EasterStart

--- app/public/pages/easter.php
+++ app/public/pages/easter.php
@@ -6,15 +6,23 @@
     </div>
   </div>
   <h2>Lent and Holy Week 2026</h2>
-  <p>Join us as we journey from Ash Wednesday to Easter Sunday. </p>
-  <ul>
-    <li>Ash Wednesday</li>
-    <li>Wednesday Lent Services</li>
-    <li>Wednesday Lent Suppers</li>
-    <li>Palm Sunday</li>
-    <li>Maundy Thursday</li>
-    <li>Good Friday</li>
-    <li>Easter Vigil</li>
-    <li>Easter Sunday</li>
-  </ul>
+    <p>Join us as we journey from Ash Wednesday to Easter Sunday. </p>  
+  <h3>Ash Wednesday</h3>
+    <p>Ash Wednesday marks the beginning of Lent, a season of reflection and repentance. Join us for our Ash Wednesday service on February 18th at 6:30 P.M., where we will receive ashes as a sign of our mortality and need for God's grace.</p>
+  <h3>Wednesday Lent Services</h3>
+    <p>Each Wednesday during Lent, we will hold a special service at 11:00 A.M. and 6:30 P.M.. These services will focus on themes of repentance, forgiveness, and renewal as we prepare our hearts for Easter.</p>
+  <h3>Wednesday Lent Suppers</h3>
+    <p>Join us for a light supper before our evening Lent services, starting at 5:00 P.M. in the Gym. This is a great opportunity to connect with fellow members of our congregation.</p>
+  <h3>Palm Sunday</h3>
+    <p>Celebrate Jesus' triumphant entry into Jerusalem on Palm Sunday, March 24th. Services will be held at 8:00 A.M. and 10:30 A.M., featuring a procession of palms branches as we start our journey in Holy Week.</p>
+  <h3>Maundy Thursday</h3>
+    <p>On March 28th, we will commemorate the Last Supper with a Maundy Thursday service at 6:30 P.M., including Holy Communion and the stripping of the altar as we remember Jesus' love for us as he gives his life for us.</p>
+  <h3>Good Friday</h3>
+    <p>Join us on April 3 at 1:00 P.M. and 6:30 P.M. for our Good Friday service, where we will reflect on the crucifixion of Jesus Christ and the profound sacrifice he made for our salvation.</p>
+  <h3>Easter Vigil</h3>
+    <p>Celebrate the resurrection of Jesus Christ at our Easter Vigil service on April 4th at sunset. This service will include the lighting of the Paschal candle, a candlelight vigil/procession, scripture readings, and the joyous proclamation of Christ's victory over death.</p>
+  <h3>Easter Sunday</h3>
+    <p>Join us on April 5th for our Easter Sunday services at 8:00 A.M. and 10:30 A.M. Celebrate the resurrection of our LORD with timeless hymns, the message of Jesus victory over the grave, and the joy of Holy Communion.</p>
+    <p>We look forward to celebrating this joyous season with you and your family. May the hope of our risen savior fill your hearts with peace and joy!</p>
+  
 </section>
